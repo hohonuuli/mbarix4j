368a9a4c2862cbd00b50ffbaa29680952440a9d7
Brian Schlining <bschlining@gmail.com>
2014-11-20 12:48:27 -0800

    Added mutable JList and DynamicList

---
+++ src/main/java/org/mbari/swing/ListCellEditor.java
@@ -0,0 +1,10 @@
+package org.mbari.swing;
+
+import javax.swing.*;
+import java.awt.*;
+
+public interface ListCellEditor extends CellEditor {
+    Component getListCellEditorComponent(JList list, Object value,
+                                          boolean isSelected, 
+                                          int index); 
+} 
