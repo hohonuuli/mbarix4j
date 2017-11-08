d73c74c740ac43c75497f9d2ea92dfc5c68ec1db
Brian Schlining <bschlining@gmail.com>
2017-11-07 16:25:57 -0800

    check point

--- src/main/java/org/mbari/util/StringUtilities.java
+++ src/main/java/org/mbari/util/StringUtilities.java
@@ -99,6 +99,35 @@ public class StringUtilities {
     public static final String repeat(String s, int i) {
       return new String(new char[i]).replace("\0", s);
     }
-    
+
+    public static boolean isBlank(final CharSequence cs) {
+        int strLen;
+        if (cs == null || (strLen = cs.length()) == 0) {
+            return true;
+        }
+        for (int i = 0; i < strLen; i++) {
+            if (!Character.isWhitespace(cs.charAt(i))) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean containsOrderedChars(final String chars, final String text) {
+        if (chars == null || text == null ) {
+            return false;
+        }
+        int idx = text.indexOf(chars.charAt(0));
+        if (idx >= 0 && chars.length() == 1) {
+            return true;
+        }
+        else if (idx >= 0) {
+            return containsOrderedChars(chars.substring(1), text.substring(idx + 1));
+        }
+        else {
+            return false;
+        }
+
+    }
 
 }
